bd1cef2bfba1edee3451a3b51456965fd84ecb86
Shane Drafahl <sdrafahl@pop-os.localdomain>
2019-08-29 20:08:03 -0500

    Adding projectile into emacs.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,3 +22,17 @@
          ([f10] . helm-buffers-list)
          ([S-f10] . helm-recentf)))
 
+(unless (package-installed-p 'projectile)
+  (package-install 'projectile))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (projectile helm use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
