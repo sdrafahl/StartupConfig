c3eb7b2cc28d79d70bf1dd90eb0e808c8105b24b
Shane <shanedrafahl@gmail.com>
2019-09-26 15:17:13 -0500

    adding ivy

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -99,6 +99,13 @@
   :config
   (editorconfig-mode 1))
 
+(ivy-mode 1)
+
+(setq ivy-use-virtual-buffers t)
+(setq ivy-count-format "(%d/%d) ")
+
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+
 (show-paren-mode 1)
 
 (menu-bar-mode -1) 
