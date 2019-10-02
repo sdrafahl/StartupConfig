c5b333be5bb93bdc960e83f00218509a82105bd8
Shane <shanedrafahl@gmail.com>
2019-10-02 17:16:55 -0500

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -38,6 +38,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(global-set-key (kbd "M-q") 'projectile-find-file)
 
 (use-package cyberpunk-theme)
 
