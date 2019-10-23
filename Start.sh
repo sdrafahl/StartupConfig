1bc44ceb6b8a3117d0e3ed66ba4772a28a623e5c
Shane <shanedrafahl@gmail.com>
2019-10-23 14:14:06 -0500

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -1,1 +1,1 @@
-sudo rm -fr StartupConfig && git clone https://github.com/sdrafahl/StartupConfig.git && mv ./StartupConfig/setup.sh setup.sh && sudo sh ./setup.sh
+rm -fr StartupConfig && git clone https://github.com/sdrafahl/StartupConfig.git && mv ./StartupConfig/setup.sh setup.sh && sh ./setup.sh
