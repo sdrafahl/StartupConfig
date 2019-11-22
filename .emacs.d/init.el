e363bcc8a689ab5535f306d8a67c4a77635f7cd5
Shane <shanedrafahl@gmail.com>
2019-11-21 18:11:58 -0600

    adding god mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -168,3 +168,16 @@
                   :server-id 'pyls))
 
 (use-package yasnippet)
+
+(use-package god-mode
+  :bind (("C-." . 'god-mode-all))
+)
+
+(defun my-update-cursor ()
+  (setq cursor-type (if (or god-local-mode buffer-read-only)
+                        'box
+                      'bar)))
+
+(add-hook 'god-mode-enabled-hook 'my-update-cursor)
+(add-hook 'god-mode-disabled-hook 'my-update-cursor)
+
