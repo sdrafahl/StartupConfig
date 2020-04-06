4c81971f6322081d3780afca5d548c8998928b13
Shane <shanedrafahl@gmail.com>
2020-04-06 17:33:27 -0500

    adding db stuff

--- setup.sh
+++ setup.sh
@@ -100,3 +100,7 @@ sudo mv kubectl /usr/local/bin/
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 sudo apt-get update && sudo apt-get install google-cloud-sdk
+
+# Installing DB stuff
+sudo apt install postgresql-client-common
+sudo apt install leiningen
