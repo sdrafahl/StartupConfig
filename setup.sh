dff2d4f5785312117fefe3d0de5289bba02e08cd
Shane <shanedrafahl@gmail.com>
2021-01-04 12:16:09 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -54,7 +54,7 @@ sudo ./coursier bootstrap \
   --java-opt -Xss4m \
   --java-opt -Xms100m \
   --java-opt -Dmetals.client=emacs \
-  org.scalameta:metals_2.12:0.9.8 \
+  org.scalameta:metals_2.12:0.9.1 \
   -r bintray:scalacenter/releases \
   -r sonatype:snapshots \
   -o /usr/local/bin/metals-emacs -f
